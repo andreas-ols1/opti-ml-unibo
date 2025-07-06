34fba2b3fe2ec36c73e44718c5716874ae9d6bd6
Andreas Omholt Olsen <andreaoo@stud.ntnu.no>
2025-07-06 23:28:36 +0200

    Rename due naming conflict

--- utils/utils.py
+++ utils/utils.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def find_outliers_iqr(
     df: pd.DataFrame, col_name: str, threshold: float = 1.5
-) -> pd.DataFrame:
+) -> pd.Series:
     Q1 = df[col_name].quantile(0.25)
     Q3 = df[col_name].quantile(0.75)
     IQR = Q3 - Q1
